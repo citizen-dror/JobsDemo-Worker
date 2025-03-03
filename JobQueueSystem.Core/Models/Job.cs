8a41aca6c72af07e6988a706f752b8f5738a90de
citizen-dror <reshef.dror@gmail.com>
2025-03-03 17:58:43 +0200

    clean class Job

--- JobQueueSystem.Core/Models/Job.cs
+++ JobQueueSystem.Core/Models/Job.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace JobQueueSystem.Core.Models
 {
     public class Job
